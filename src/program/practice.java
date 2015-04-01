008473c8b6955d7eabe34fab4ca5e7a24262b0e2
cesarvg <alexvg714@yahoo.com>
2015-03-31 17:54:22 -0700

    first commit

---
+++ src/program/practice.java
@@ -0,0 +1,18 @@
+package program;
+
+import javax.swing.JOptionPane; 
+
+public class practice {
+	
+public static void main (String[] args )
+{
+	string userName;
+	
+	userName = JOptionPane.showInputDialog("whats your name?");
+			
+	
+	JOptionPane.showMessageDialog(null,  "hey" + userName);
+	
+	
+}
+}
